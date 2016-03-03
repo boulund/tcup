a5433fa4afa6906325b0b90c12481b4a6a9aa9e0
Fredrik Boulund <fredrik.boulund@chalmers.se>
2016-03-03 22:26:48 +0100

    Updated parser to use sqlite3 resfinder db that can be created with construct_resfinder_db.py

---
+++ construct_resfinder_db.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3.5
+# Construct an easy-to-use ResFinder db
+# Fredrik Boulund 2016
+
+from sys import argv, exit
+import difflib
+import argparse
+import logging
+import sqlite3
+import os
+import re
+
+
+def parse_commandline():
+    """
+    Parse commandline.
+    """
+
+    desc = """Construct ResFinder sqlite3 DB. Fredrik Boulund 2016"""
+
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("-s", "--sequences", dest="sequences",
+            help="File with sequences in FASTA format.")
+    parser.add_argument("-n", "--notes", dest="notes",
+            help="File called 'notes.txt' from ResFinder.")
+    parser.add_argument("-d" "--db", dest="dbfile",
+            default="resfinder.sqlite3",
+            help="Database filename for sqlite3 database [%(default)s].")
+    parser.add_argument("--use-closest-match", dest="use_closest_match",
+            action="store_true",
+            default=False,
+            help="Use closest match if unable to map gene symbol parsed from FASTA header to ResFinder's notes.txt [%(default)s].")
+    parser.add_argument("--loglevel", 
+        choices=["INFO", "DEBUG"],
+        default="DEBUG",
+        help="Set logging level [%(default)s].")
+
+    if len(argv)<2:
+        parser.print_help()
+        exit()
+
+    options = parser.parse_args()
+
+    logging.basicConfig(level=options.loglevel)
+    return options
+
+
+def parse_headers_from_fasta(filename):
+    """
+    Parse headers from fasta file.
+
+    Shows a warning if identical headers are seen.
+    """
+    already_seen = set()
+    with open(filename) as f:
+        for line in f:
+            if line.startswith(">"):
+                header = line[1:].strip()
+                if header in already_seen:
+                    logging.warning("Header %s seen (at least) twice.", header)
+                yield header
+                already_seen.add(header)
+
+
+def parse_resfinder_notes(filename):
+    """
+    Parse database mappings from ResFinder notes.txt.
+    """
+    mappings = {}
+    with open(filename) as f:
+        for count, line in enumerate(f, start=1):
+            if line.startswith("#"):
+                continue
+            try:
+                symbol, resistance_type, extra = line.strip().split(":")
+            except ValueError:
+                logging.error("Line %i cannot be parsed: %s", count, line)
+            mappings[symbol] = (resistance_type, extra)
+    return mappings
+
+
+def merge_fasta_headers_and_notes(headers, notes, use_closest_match=False):
+    """
+    Merge FASTA headers and information from ResFinder notes.txt.
+
+    Yields a stream of (header, symbol, family, class, extra) tuples.
+    """
+
+    def guess_family(s):
+        """ Guess the AR family of a sequence header in ResFinder. """
+        if s.count("-") > 1:
+            s = s.rsplit("-", 1)[0]
+        return s.rstrip("0123456789-")
+
+    missing_mappings = []
+
+    for header in headers:
+        symbol = header.split("_", 1)[0]
+        family = guess_family(symbol)
+        #logging.debug("Guessed family %s for header %s", family, header)
+        try:
+            yield (header, symbol, family, notes[symbol][0], notes[symbol][1])
+            continue
+        except KeyError:
+            if use_closest_match:
+                closest_matching_symbol = difflib.get_close_matches(symbol, notes.keys(), n=1, cutoff=0.6)[0]
+                if len(closest_matching_symbol) > 0:
+                    logging.warning("Using '%s' as closest matching symbol for header '%s'", closest_matching_symbol, header)
+                    infostring = " WARNING: AUTOMATICALLY ASSIGNED AS CLOSEST MATCH FOR HEADER {}".format(header)
+                    yield (header, 
+                           symbol, 
+                           family, 
+                           notes[closest_matching_symbol][0], 
+                           notes[closest_matching_symbol][1]+infostring)
+                    continue
+            else:
+                logging.warning("Symbol '%s' from header '%s' not found in %s", symbol, header, options.notes)
+            missing_mappings.append(header)
+    if len(missing_mappings) > 0:
+        logging.warning("You should manually enter the %s missing mappings into db.", len(missing_mappings))
+    return missing_mappings
+
+
+def create_resfinder_sqlite3_db(dbfile, mappings):
+    """
+    Create and fill an sqlite3 DB with ResFinder mappings.
+
+    Expects mappings to be a list of tuples:
+      (header, symbol, class, extra)
+    """
+
+    logging.info("Creating sqlite3 db: %s ...", dbfile)
+    if os.path.isfile(dbfile):
+        logging.warning("Overwriting previously existing dbfile: %s")
+        os.remove(dbfile)
+        logging.debug("Removed pre-existing dbfile: %s")
+    con = sqlite3.connect(dbfile)
+    con.execute("CREATE TABLE resfinder(header TEXT PRIMARY KEY, symbol TEXT, family TEXT, class TEXT, extra TEXT)")
+    con.executemany("INSERT INTO resfinder VALUES (?,?,?,?,?)", mappings)
+    num_mappings = con.execute("SELECT Count(*) FROM resfinder").fetchone()[0]
+    con.commit()
+    logging.debug("Inserted %i mappings in to sqlite3 DB", num_mappings)
+    return con
+
+
+def main(options):
+    """
+    Main.
+    """
+
+    headers = set(parse_headers_from_fasta(options.sequences))
+    db_mappings = parse_resfinder_notes(options.notes)
+    mapping_tuples = merge_fasta_headers_and_notes(headers, db_mappings, options.use_closest_match)
+    create_resfinder_sqlite3_db(options.dbfile, mapping_tuples)
+
+
+if __name__ == "__main__":
+    options = parse_commandline()
+    main(options)
--- parse_AR_blast8.py
+++ parse_AR_blast8.py
@@ -2,11 +2,12 @@
 # Parse AR hits from pBLAT blast8
 # Fredrik Boulund 2016
 
-from sys import argv, exit
-from collections import defaultdict
+from sys import argv, exit, stdout
+from collections import defaultdict, Counter
 from itertools import groupby
 import argparse
 import logging
+import sqlite3
 from os import path, makedirs
 
 
@@ -19,7 +20,15 @@ def parse_commandline():
 
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument("FILE", type=str, nargs="+",
-        help="Filename of pBLAT blast8 file to parse.")
+            help="pBLAT blast8 file to parse.")
+    parser.add_argument("-r", "--resfinder", dest="resfinder",
+            help="ResFinder sqlite3 database.")
+    parser.add_argument("-m", "--min-identity", dest="min_identity",
+            type=float,
+            default=100.00,
+            help="Minimum identity for pBLAT matches [%(default)s].")
+    parser.add_argument("-o", "--output", dest="output", metavar="OUTFILE",
+            help="Write output to OUTFILE.")
     parser.add_argument("--loglevel", 
         choices=["INFO", "DEBUG"],
         default="INFO",
@@ -35,7 +44,7 @@ def parse_commandline():
     return options
 
 
-def parse_blat_output(filename, max_pid_diff=5):
+def parse_blat_output(filename, min_identity=100, max_pid_diff=0):
     """
     Filter out the best hit(s) for each query sequence.
     
@@ -62,7 +71,10 @@ def parse_blat_output(filename, max_pid_diff=5):
             query = hit[0]
             target = hit[1]
             pid = float(hit[2])
+            if pid < min_identity:
+                continue
             hitlists[query].append((target, pid))
+
         try:
             logging.info("Parsed %s hits.", total_count)
         except UnboundLocalError:
@@ -81,16 +93,54 @@ def parse_blat_output(filename, max_pid_diff=5):
         logging.info("%s hits for %s peptides remain after relative filtering.", num_remain_hits, num_remain_pep)
 
 
+class ResFinderDB():
+    """
+    Convenience wrapper for ResFinder sqlite3 database.
+    """
+
+    def __init__(self, filename):
+        self.dbfile = filename
+        self.db = sqlite3.connect(self.dbfile)
+        logging.debug("Connected to sqlite3 db: %s", self.dbfile)
+        
+
+    def __getitem__(self, key):
+        """ Overload getter: retrieve row from db using ResFinderDB["header"] """
+        header = self.db.execute("SELECT * FROM resfinder WHERE header = ?", (key,)).fetchone()
+        return header
+
+
+def best_matching_family_per_peptide(blast8file, min_identity, resfinder_db):
+    """
+    Yield the best matching family per peptide.
+    """
+
+    resfinder = ResFinderDB(resfinder_db)
+
+    for query, hits in groupby(parse_blat_output(blast8file, min_identity, max_pid_diff=0), key=lambda x: x[0]):
+        matching_targets = (target for _, target, _ in hits)
+        matching_families = set(resfinder[target][2] for target in matching_targets)
+        if len(matching_families) == 1:
+            yield list(matching_families)[0]  # set is not indexable
+
+
 def main(options):
     """
     Main.
     """
-    for blast8file in options.FILE:
-        for query, hits in groupby(parse_blat_output(blast8file, max_pid_diff=0), key=lambda x: x[0]):
-            print(query)
-            for q, target, pid in hits:
-                print("  {}".format(target))
-                pass
+
+    if options.output:
+        outfilehandle = open(options.output, 'w')
+    else:
+        outfilehandle = stdout
+
+    with outfilehandle as outfile:
+        for blast8file in options.FILE:
+            family_counter = Counter(best_matching_family_per_peptide(blast8file, options.min_identity, options.resfinder))
+            print("Results for {}".format(blast8file),  file=outfile)
+            print("{:<10} {}".format("Count", "Family"), file=outfile)
+            for family, count in family_counter.most_common():
+                print("{:<10} {}".format(count, family), file=outfile)
 
 
 if __name__ == "__main__":
